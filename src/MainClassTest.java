433b2d8d8daefc6b3eedca8a3b34accc4c67e121
Pivovarova Aleksandra <Pivovarova.AO@dns-shop.ru>
2021-07-15 09:39:11 +1000

    Лекция 1. Тест 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,11 @@ public class MainClassTest extends MainClass
         int number = getLocalNumber();
         Assert.assertEquals("Число не равно ожидаемому!",14, number);
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        int number = getClassNumber();
+        Assert.assertTrue("Число не больше заданного!", number > 45);
+    }
 }
